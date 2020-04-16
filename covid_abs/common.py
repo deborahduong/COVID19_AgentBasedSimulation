bd8a5c1d2579b0c5d3c85d4579276a512a686fcc
Deborah Duong <deborahvakasduong@gmail.com>
2020-04-16 07:10:13 -0700

    Update common.py

--- covid_abs/common.py
+++ covid_abs/common.py
@@ -6,7 +6,7 @@ Probability of each infection serverity by age
 
 Source: Imperial College Covid-19 research team
 """
-age_symptomatic_probs=  [0.01, 0.025, 0.05, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6 ]
+age_symptomatic_probs=  [0.005, 0.01, 0.025, 0.05, 0.1, 0.15, 0.2, 0.25, 0.3 ]
 age_hospitalization_probs = [0.001, 0.003, 0.012, 0.032, 0.049, 0.102, 0.166, 0.243, 0.273 ]
 age_severe_probs = [0.05, 0.05, 0.05, 0.05, 0.063, 0.122, 0.274, 0.432, 0.709 ]
 age_death_probs = [0.002, 0.00006, 0.0003, 0.0008, 0.0015, 0.006, 0.022, 0.051, 0.093 ]
